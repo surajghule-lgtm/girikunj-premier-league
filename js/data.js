cce9b833505feab76d22429a599f59b5fe837607
surajghule-lgtm <surajghuleqa1@gmail.com>
2026-01-17 16:47:46 +0530

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,25 @@
+// CENTRAL MATCH DATA (used everywhere)
+
+const tournament = {
+  liveMatch: {
+    isLive: true,
+    matchId: 1,
+    teamA: "Girikunj Lions",
+    teamB: "Girikunj Tigers",
+    runs: 32,
+    wickets: 2,
+    overs: "3.1"
+  },
+
+  results: [],
+
+  players: {
+    "Ayaan Mishra": { runs: 0, wickets: 0 },
+    "Ajay Singh": { runs: 0, wickets: 0 }
+  },
+
+  pointsTable: {
+    "Girikunj Lions": { played: 0, won: 0, lost: 0, points: 0, nrr: 0 },
+    "Girikunj Tigers": { played: 0, won: 0, lost: 0, points: 0, nrr: 0 }
+  }
+};
